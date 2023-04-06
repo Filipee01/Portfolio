c80f35c0c0153666c1e2cc82e8ed2abe1b3985fd
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2023-04-06 12:28:25 -0300

    Atualização 2023

--- main.js
+++ main.js
@@ -9,16 +9,16 @@
   
   window.sr = ScrollReveal({ reset: true});
   
-  sr.reveal('.wrapp-content', {duration: 2000})
-  sr.reveal('.image-profile', { rotate:{ x:0, y:80, z:-20 }, duration: 2000 })
-  sr.reveal('.wrapp-contain', { duration: 2000})
-  sr.reveal('.skills-c', { duration: 2000})
-  sr.reveal('.skills-c', { duration: 2000})
-  sr.reveal('.images-skills', { duration: 2000})
-  sr.reveal('.learning', { duration: 2000})
-  sr.reveal('.wrapp-images-bot', { duration: 2000})
-  sr.reveal('#pro', { duration: 2000})
-  sr.reveal('.projects-git', { duration: 2000})
+  sr.reveal('.wrapp-content', {duration: 1500})
+  sr.reveal('.image-profile', { rotate:{ x:0, y:80, z:-20 }, duration: 1500 })
+  sr.reveal('.wrapp-contain', { duration: 1500})
+  sr.reveal('.skills-c', { duration: 1500})
+  sr.reveal('.skills-c', { duration: 1500})
+  sr.reveal('.images-skills', { duration: 1500})
+  sr.reveal('.learning', { duration: 1500})
+  sr.reveal('.wrapp-images-bot', { duration: 1500})
+  sr.reveal('#pro', { duration: 1500})
+  sr.reveal('.projects-git', { duration: 1500})
   
 
 
